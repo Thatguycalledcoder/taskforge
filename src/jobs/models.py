6b1dbf25ca052d2e534e0ca1071d7633c670c6fb
David <david@Davids-MacBook-Pro.local>
2026-08-23 18:09:51 +0000

    Updated jobs result field to be optional.

--- src/jobs/models.py
+++ src/jobs/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import DateTime, ForeignKey, Integer, String, Text, Enum as SqlE
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import JSONB
-from typing import Any
+from typing import Any, Optional
 from auth.models import User
 from datetime import datetime
 from enum import Enum
@@ -25,7 +25,7 @@ class Job(Base):
     status: Mapped[JobStatus] = mapped_column(SqlEnum(JobStatus), nullable=False, default=JobStatus.PENDING)
 
     payload: Mapped[dict[str, Any]] = mapped_column(JSONB, nullable=False, default=dict)
-    result: Mapped[dict[str, Any]] = mapped_column(JSONB, nullable=False, default=dict)
+    result: Optional[Mapped[dict[str, Any]]] = mapped_column(JSONB, nullable=False, default=dict)
 
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
