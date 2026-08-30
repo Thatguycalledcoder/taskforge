014bb8d55233d3e50d14ba5cf3b8f05f11180c86
David <david@Davids-MacBook-Pro.local>
2026-08-30 14:01:41 +0000

    Updated database.py following v2 standard.

--- src/core/database.py
+++ src/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from core.config import settings
 
@@ -7,12 +7,10 @@ engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 def get_db():
-    db = SessionLocal()
-    try:
+    with SessionLocal() as db:
         yield db
-    finally:
-        db.close()
